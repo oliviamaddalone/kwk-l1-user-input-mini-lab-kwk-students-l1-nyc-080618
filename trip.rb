fe09b48e1947b778ad234ca02b3dfd85d30cea5c
oliviamaddalone <livmaddalone@gmail.com>
2018-08-06 19:50:34 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -2,7 +2,6 @@
 
 # First, puts out a string asking where the tourist would like to stay.
 puts "Let's plan your trip! Where would you like to stay?"
-stay = gets.chomp
 
 # Then, set a variable called stay to get the user's input. Make sure to capitilize that input.
 
