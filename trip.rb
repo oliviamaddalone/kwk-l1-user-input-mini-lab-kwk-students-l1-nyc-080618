2f1bbfab44978b8f0bf8f07673baab31e7ddadc6
oliviamaddalone <livmaddalone@gmail.com>
2018-08-06 19:33:05 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -17,7 +17,7 @@ puts "Let's plan your trip! Where would you like to stay?"
 stay = gets.chomp.capitalize
 
 puts "Where do you like to eat?"
-eat = pizza
+eat = gets.chomp 
 
 puts "Where do you spend your nights?"
 nights = the beach
